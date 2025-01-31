02a4908c7580c1fac098fe8df9a34b0813ccce94
egan-ombaka <eganombaka97@gmail.com>
2025-01-31 16:48:45 +0300

    Variables & comments

---
+++ Crash Learning(python)/variables&comments.py
@@ -0,0 +1,11 @@
+#This is a commment
+
+x = 5
+y = "Hello, world!"
+
+#Variables do not need to be declared with any particular type, and can even change type after they have been set.
+#VAriables are case sensitive
+
+a = 4
+A = "Sally"
+#A will not overwrite a
